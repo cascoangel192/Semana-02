9f071b8cd74b449295e37d688630acc3c354757b
Ángel Casco <solo paja sos vos@gmail.com>
2026-09-17 23:22:23 -0600

    commit 2

--- Variables.py
+++ Variables.py
@@ -1,4 +1,8 @@
 # Esta sesion será para aprender el uso de variables
-print("hola mundo")
-print("hoy es miercoles")
-print("mañana es jueves")
+print("Sistema de mensajeria")
+distancia_km = 6.5
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
+
+cobro = peso_kg * cobro_por_kg 
